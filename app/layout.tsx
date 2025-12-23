615b48dc0434fabad8cf9a9f2e0e03647945442b
Porygon <porygonsol@gmail.com>
2025-12-23 20:52:30 +0530

    open graph changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,28 @@ export const metadata: Metadata = {
     shortcut: '/icon.png',
     apple: '/icon.png',
   },
+  openGraph: {
+    title: 'Porygon',
+    description: 'We build websites that turn visitors into paying customers. Strategic design, SEO, and development.',
+    url: 'https://porygonsol.com',
+    siteName: 'Porygon',
+    images: [
+      {
+        url: '/icon.png',
+        width: 1200,
+        height: 630,
+        alt: 'Porygon - Web Development Solutions',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Porygon',
+    description: 'We build websites that turn visitors into paying customers. Strategic design, SEO, and development.',
+    images: ['/icon.png'],
+  },
 }
 
 export default function RootLayout({
